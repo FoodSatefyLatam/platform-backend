e43166ea54219c0125185b5fa3a3c17449f46188
F3lIX99 <felixtorresmuena@gmail.com>
2022-10-27 18:06:58 -0300

    ahora sim

--- calculadora.py
+++ calculadora.py
@@ -2,15 +2,23 @@
 @app.route("/calculadora", methods = ["POST"])
 def calculadora():
     try:
-    weight = 0
-    height = 0
-    food = " "   
+        weight = 0
+        amount = 0
+        food = " "   
         request_json = request.get_json()
-        if(request_json.get("weight"))
+        if(request_json.get("weight")):
             weight = request_json["weight"]
-        if(request_json.get("height"))
-            height = request_json["height"]
-        if(request_json.get("food"))
+        if(request_json.get("amount")):
+            amount = request_json["amount"]
+        if(request_json.get("food")):
             food = request_json["food"]
+
+    except:
+        print("request error")
+
+    #aqui saca las weas del contaminante
+    cur = mysql.connection.cursor()
+    cur.execute("")
+    formula = (amount * contaminate)/(valor_referencia * weight)    
 
         
