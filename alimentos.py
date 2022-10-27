2acfda51a524cb896a0645b82824f869aaa83242
Cheloo99 <104237374+Cheloo99@users.noreply.github.com>
2022-10-27 18:01:07 -0300

    Cambios en alimentos.py

--- alimentos.py
+++ alimentos.py
@@ -7,9 +7,9 @@ def request_alimentos():
     cur = mysql.connection.cursor()
     cur.execute("SELECT especie FROM alimento")
     #consulta
-    lista_alimentos = []
+    lista_alimentos = ""
     alimentos = cur.fetchall()
     for alimento in alimentos:
-        lista_alimentos.append(alimento[0]);
+        lista_alimentos += alimento[0] + ";" 
         print(alimento[0])
     return lista_alimentos
