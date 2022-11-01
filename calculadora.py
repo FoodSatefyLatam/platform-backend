8b6e1b56677e1a9c058cabffce9746375b975889
Cheloo99 <104237374+Cheloo99@users.noreply.github.com>
2022-11-01 17:01:29 -0300

    Cambios finales calculadora

--- calculadora.py
+++ calculadora.py
@@ -27,14 +27,13 @@ def calculadora():
     valor_referencia = cur.fetchone()
     cur.execute("SELECT id_contaminante FROM contaminante WHERE nombre= %s",[contaminante])
     id_contaminante=cur.fetchone()
-    alimento="albacora"
-    cur.execute("SELECT id_alimento FROM alimento WHERE especie=%s",[alimento]) #para probar se uso la albacora
+    cur.execute("SELECT id_alimento FROM alimento WHERE especie=%s",[food]) 
     id_alimento=cur.fetchone()
     cur.execute("SELECT Avg(cantidad)  FROM  muestreo WHERE id_contaminante=%s AND id_alimento=%s" ,([id_contaminante],[id_alimento]))
     promedio=cur.fetchone()
     print (promedio[0])
     print (valor_referencia[0])
-    formula = (11 * float(promedio[0]))/(float(valor_referencia[0]) * 70)
+    formula = (amount * float(promedio[0]))/(float(valor_referencia[0]) * weight) #amount es la cantidad de alimento y weight el peso de la persona
     if formula < 1.0:
         return "Dentro de lo normal"
     else:
