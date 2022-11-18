d4ba1e871f67469253eb50da3c6c8da059445117
BFernandezV <bfernandez@inf.udec.cl>
2022-11-18 10:00:03 -0300

    CORS Added

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'abcd.1234'
+app.config['MYSQL_USER'] = 'benjaminF'
+app.config['MYSQL_PASSWORD'] = '123456'
 app.config['MYSQL_DB'] = 'proyectocontaminantes'
 mysql = MySQL(app)
 
