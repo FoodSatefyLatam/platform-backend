480c26950cd3f28737ab96b19535e4ef4b7d3611
jonathanavc <jonvenega@gmail.com>
2022-11-28 01:22:19 -0300

    Update alimentos.py

--- alimentos.py
+++ alimentos.py
@@ -2,13 +2,13 @@ from flask import jsonify
 
 from __main__ import app, mysql
 
-@app.route("/request_alimentos")
-def request_alimentos():
+@app.route("/alimentos")
+def alimentos():
     cur = mysql.connection.cursor()
     cur.execute("SELECT especie FROM alimento")
     #consulta
     lista_alimentos = []
-    alimentos = cur.fetchall()
-    for alimento in alimentos:
+    _alimentos = cur.fetchall()
+    for alimento in _alimentos:
         lista_alimentos.append(alimento[0])
     return jsonify(lista_alimentos)
