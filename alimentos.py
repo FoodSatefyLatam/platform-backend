99b3fc20fb90466eedd9a7a1a1f8bf4e22a454a9
jonathanavc <jonvenega@gmail.com>
2022-11-28 15:52:11 -0300

    Update alimentos.py

--- alimentos.py
+++ alimentos.py
@@ -16,14 +16,15 @@ def alimentos():
         request_json = request.get_json()
         contaminantes = []
         if(request_json.get("contaminantes")):
-            contaminantes = request_json.get("contaminantes")
-            for contaminante in contaminantes:
-                cur = mysql.connection.cursor()
-                cur.execute("SELECT especie FROM alimento LEFT JOIN muestreo WHERE nombre = %s AND cantidad != 0",[contaminante])
-                _alimentos = cur.fetchall()
-            for alimento in _alimentos:
-                dict_alimentos[alimento[0]] = True
+            #contaminantes = request_json.get("contaminantes")
+            print(contaminantes)
+            #for contaminante in contaminantes:
+            #    cur = mysql.connection.cursor()
+            #    cur.execute("SELECT especie FROM alimento LEFT JOIN muestreo WHERE nombre = %s AND cantidad != 0",[contaminante])
+            #    _alimentos = cur.fetchall()
+            ##for alimento in _alimentos:
+            #   dict_alimentos[alimento[0]] = True
 
-        lista_alimentos = dict_alimentos.keys
+        #lista_alimentos = dict_alimentos.keys
 
     return jsonify(lista_alimentos)
