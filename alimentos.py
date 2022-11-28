927589239ee9e0df297419c7626388b658c8c909
jonathanavc <jonvenega@gmail.com>
2022-11-28 15:55:13 -0300

    Update alimentos.py

--- alimentos.py
+++ alimentos.py
@@ -2,9 +2,9 @@ from __main__ import app, mysql, jsonify, request
 
 @app.route("/alimentos", methods=["GET", "POST"])
 def alimentos():
+    cur = mysql.connection.cursor()
     lista_alimentos = []
     if request.method == 'GET':
-        cur = mysql.connection.cursor()
         cur.execute("SELECT especie FROM alimento")
         #consulta
         _alimentos = cur.fetchall()
@@ -16,16 +16,15 @@ def alimentos():
         request_json = request.get_json()
         contaminantes = []
         if(request_json.get("contaminantes")):
-            #contaminantes = request_json.get("contaminantes")
-            print(contaminantes)
-            print("hola")
-            #for contaminante in contaminantes:
-            #    cur = mysql.connection.cursor()
-            #    cur.execute("SELECT especie FROM alimento LEFT JOIN muestreo WHERE nombre = %s AND cantidad != 0",[contaminante])
-            #    _alimentos = cur.fetchall()
-            ##for alimento in _alimentos:
-            #   dict_alimentos[alimento[0]] = True
+            for contaminante in request_json.get("contaminantes"):
+                contaminantes.append(contaminante)
+            for contaminante in contaminantes:
+                cur = mysql.connection.cursor()
+                cur.execute("SELECT especie FROM alimento LEFT JOIN muestreo WHERE nombre = %s AND cantidad != 0",[contaminante])
+                _alimentos = cur.fetchall()
+            for alimento in _alimentos:
+                dict_alimentos[alimento[0]] = True
 
-        #lista_alimentos = dict_alimentos.keys
+        lista_alimentos = dict_alimentos.keys
 
     return jsonify(lista_alimentos)
