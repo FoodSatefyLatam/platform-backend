1436bf98a0a754e440b5d64f79ce962aa639c36b
jonathanavc <jonvenega@gmail.com>
2022-11-28 18:23:05 -0300

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -24,4 +24,4 @@ def reporte():
         #MySQLdb.ProgrammingError: (1064, "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'JOIN consumo ON persona.id_folio=consumo.id_folio JOIN alimento ON consumo.id_al' at line 1")
         cur.execute("SELECT * FROM (SELECT * FROM persona WHERE sexo=%s AND edad > %s AND edad < %s AND peso > %s AND peso < %s AND altura > %s AND altura < %s) AS p LEFT JOIN consumo ON p.id_folio=consumo.id_folio LEFT JOIN alimento ON consumo.id_alimento=alimento.id_alimento LEFT JOIN muestreo ON p.id_region=muestreo.id_region LEFT JOIN contaminante ON contaminante.id_contaminante=muestreo.id_contaminante WHERE contaminante.nombre = %s",[sexo,min_edad,max_edad,min_peso,max_peso,min_altura,max_altura,contaminante])
         reporte[contaminante] = cur.fetchall()
-    return reporte
+    return jsonify(reporte)
