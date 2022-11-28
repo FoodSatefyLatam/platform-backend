fc75d544bc1908caedf36087dcd20b9a87f0d150
jonathanavc <jonvenega@gmail.com>
2022-11-28 18:16:48 -0300

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -6,36 +6,20 @@ def reporte():
         return "error"
     reporte = {}
     cur = mysql.connection.cursor()
-    sexo = 0
-    min_edad = 0
-    max_edad = 0
-    min_peso = 0
-    max_peso = 1000
-    min_altura = 0
-    max_altura = 1000
     contaminantes = []
     alimentos = []
     request_json = request.get_json()
-    if request_json.get("sexo"):
-        sexo = request_json["sexo"]
-    if request_json.get("min_age"):
-        min_edad = request_json["min_edad"]
-    if request_json.get("max_age"):
-        max_edad = request_json["max_edad"]
-    if request_json.get("min_peso"):
-        min_peso = request_json["min_peso"]
-    if request_json.get("max_peso"):
-        max_peso = request_json["max_peso"]
-    if request_json.get("min_altura"):
-        min_altura = request_json["min_altura"]
-    if request_json.get("max_altura"):
-        max_altura = request_json["max_altura"]
-    if request_json.get("contaminantes"):
-        for contaminante in request_json.get("contaminantes"):
-            contaminantes.append(contaminante)
-    if request_json.get("alimentos"):
-        for alimento in request_json.get("alimentos"):
-            alimentos.append(alimento)
+    sexo = request_json["sexo"]
+    min_edad = request_json["min_edad"]
+    max_edad = request_json["max_edad"]
+    min_peso = request_json["min_peso"]
+    max_peso = request_json["max_peso"]
+    min_altura = request_json["min_altura"]
+    max_altura = request_json["max_altura"]
+    for contaminante in request_json.get("contaminantes"):
+        contaminantes.append(contaminante)
+    for alimento in request_json.get("alimentos"):
+        alimentos.append(alimento)
     for contaminante in contaminantes:
         print(contaminante)
         #MySQLdb.ProgrammingError: (1064, "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'JOIN consumo ON persona.id_folio=consumo.id_folio JOIN alimento ON consumo.id_al' at line 1")
