a31a503c195b418dd055ff5a447ec98daad2b616
jonathanavc <jonvenega@gmail.com>
2022-12-04 22:12:08 -0300

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -17,12 +17,12 @@ def reporte():
     alimentos = request_json.get("alimentos")
     contaminantes = request_json.get("contaminantes")
     for alimento in alimentos:
+        reporte[alimento] = []
         for contaminante in contaminantes: 
             cur.execute("SELECT AVG(p.peso), AVG(consumo.cantidad) FROM (SELECT * FROM persona WHERE sexo=%s AND edad > %s AND edad < %s AND peso > %s AND peso < %s AND altura > %s AND altura < %s) AS p LEFT JOIN consumo ON p.id_folio=consumo.id_folio LEFT JOIN alimento ON consumo.id_alimento=alimento.id_alimento WHERE alimento.especie=%s",[sexo,min_edad,max_edad,min_peso,max_peso,min_altura,max_altura,alimento])
             avgs = cur.fetchall()
             peso_promedio = avgs[0][0]
             consumo_promedio = avgs[0][0]
-
             print("["+ alimento +"]["+ contaminante + "]")
             reporte[alimento][contaminante] = consumo_promedio
     return jsonify(reporte)
