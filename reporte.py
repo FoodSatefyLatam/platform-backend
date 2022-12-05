acf02654095934923bc9a9323f3d146fd16ce586
jonathanavc <jonvenega@gmail.com>
2022-12-04 22:38:46 -0300

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -20,7 +20,7 @@ def reporte():
     id_contaminantes = {}
     for contaminante in contaminantes: 
         cur.execute("SELECT valor_referencia, id_contaminante FROM contaminante WHERE nombre= %s",[contaminante]) #de momento se trabaja con el cadmio
-        res = cur.fetchone()
+        res = cur.fetchall()
         valores_referencia[contaminante] = res[0][0]
         id_contaminantes[contaminante] = res[0][1]
 
