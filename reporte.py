5c6bff34d50ae7f6eb71487036eec76bef522720
jonathanavc <jonvenega@gmail.com>
2022-12-04 22:00:23 -0300

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -14,8 +14,10 @@ def reporte():
     max_peso = request_json["max_peso"]
     min_altura = request_json["min_altura"]
     max_altura = request_json["max_altura"]
-    for alimento in request_json.get("alimentos"):
-        for contaminate in request_json.get("Contaminantes"):
+    alimentos = request_json.get("alimentos")
+    contaminantes = request_json.get("alimentos")
+    for alimento in alimentos:
+        for contaminate in contaminantes:
             cur.execute("SELECT AVG(p.peso), AVG(consumo.cantidad) FROM (SELECT * FROM persona WHERE sexo=%s AND edad > %s AND edad < %s AND peso > %s AND peso < %s AND altura > %s AND altura < %s) AS p LEFT JOIN consumo ON p.id_folio=consumo.id_folio LEFT JOIN alimento ON consumo.id_alimento=alimento.id_alimento WHERE alimento.especie=%s",[sexo,min_edad,max_edad,min_peso,max_peso,min_altura,max_altura,alimento])
             avgs = cur.fetchall()
             peso_promedio = avgs[0][0]
