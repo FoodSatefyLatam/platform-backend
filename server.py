b68e06dbe771cbd3c5be24805973373d5dbac571
Tomas Bravo <tomas.b.c@outlook.com>
2023-04-26 22:42:05 -0400

    sacando datos de db desde .env

--- server.py
+++ server.py
@@ -1,17 +1,26 @@
-from flask import Flask, request, jsonify
-from flask_mysqldb import MySQL
+import os
+from logging import debug
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_mysqldb import MySQL
+
+import alimentos
+import calculadora
+import contaminantes
+import reporte
+
+load_dotenv('.env')
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'grupo1'
-app.config['MYSQL_PASSWORD'] = 'gq0xf7vk'
-app.config['MYSQL_DB'] = 'grupo1'
+app.config['MYSQL_HOST'] = os.environ.get('DB_HOST')
+app.config['MYSQL_USER'] = os.environ.get('DB_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('DB_PASS')
+app.config['MYSQL_DB'] = os.environ.get('DB_NAME')
 mysql = MySQL(app)
 
-import alimentos, calculadora, reporte, contaminantes
-
 if __name__ == "__main__":
-    app.run(port = '5001',  host= '0.0.0.0', debug=True, threaded=True)
+    app.run(port=5001,  host='0.0.0.0', debug=True, threaded=True)
