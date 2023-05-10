ecd03db744aa76975412786bcb6a9bcd47ceb2d5
Nacho <nacho@localhost.localdomain>
2023-05-10 17:32:10 +0000

    update extraer_consumo.py

--- scripts/extraer_consumo.py
+++ scripts/extraer_consumo.py
@@ -19,6 +19,8 @@ cursor = mydb.cursor()
 col_consumo = ["folio", "g_producto", "consumo_mes", "mg_ml"]
 df_consumo_alimentos = pd.read_csv("../../csv/ENCA_ETCC_ALIMENTOS_INDIVIDUALES.csv", sep=",", header=0, usecols=col_consumo, encoding="ISO-8859-1")
 
+
+
 # Imprimir el DataFrame
 print(df_consumo_alimentos)
 
@@ -27,12 +29,18 @@ sql_consumo = "INSERT INTO Consumo(id_persona, id_alimento, cantidad, cantidad_m
 
 for index, row in df_consumo_alimentos.iterrows():
     
+
+
     # Obtener el id_alimento a partir del g_producto
     cursor.execute("SELECT id FROM Alimento WHERE nombre=%s", (row["g_producto"],))
     id_alimento = cursor.fetchall()[0][0]
     
     print(id_alimento)
 
+
+    # ROW["col"].isnull() ? 0 : ROW["col"]
     # Insertar la fila en la tabla Consumo
-    cursor.execute(sql_consumo, (row["folio"], id_alimento, row["consumo_mes"], row["mg_ml"]))
+
+    
+    cursor.execute(sql_consumo, (row["folio"], id_alimento, row["consumo_mes"].isnull() ? 0 : row["consumo_mes"], row["mg_ml"].isnull() ? 0 : row["mg_ml"]))
     mydb.commit()
