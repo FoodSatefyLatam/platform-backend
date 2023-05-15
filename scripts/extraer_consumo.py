b04153d3e6f7f52f3c4473f42359d34f1eeb07ee
José Chavarría <112492450+NachoCHR@users.noreply.github.com>
2023-05-15 23:08:30 +0000

    print homologados strip lower

--- scripts/extraer_consumo.py
+++ scripts/extraer_consumo.py
@@ -22,7 +22,7 @@ df_consumo_alimentos = pd.read_csv("../../csv/ENCA_ETCC_ALIMENTOS_INDIVIDUALES.c
 
 
 # Imprimir el DataFrame
-print(df_consumo_alimentos)
+print(row["homologados"].strip().lower())
 
 # Inserción de datos en la tabla Consumo
 sql_consumo = "INSERT INTO Consumo(id_persona, id_alimento, cantidad, cantidad_mes) VALUES (%s, %s, %s, %s)"
