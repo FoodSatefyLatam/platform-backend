83338d21472f8a0b790293b536e77683cf74e491
José Chavarría <112492450+NachoCHR@users.noreply.github.com>
2023-05-16 02:25:52 +0000

    Update extraer_consumo.py

--- scripts/extraer_consumo.py
+++ scripts/extraer_consumo.py
@@ -60,7 +60,9 @@ for index, row in df_consumo_alimentos.iterrows():
     else :
         print("Entre porque id alimento: ",id_alimento)
         cantidad = consumo[2] + row["mg_ml"]
+        print("cantidad:", cantidad)
         cantidad_mes = consumo[3] + row["consumo_mes"]
+        print("cantidad_mes",cantidad_mes)
         cursor.execute("UPDATE Consumo Set cantidad = %s , cantidad_mes = %s WHERE id_persona = %s AND id_alimento = %s",(cantidad,cantidad_mes,row["folio"],id_alimento))
     # cursor.execute(sql_consumo, (row["folio"], id_alimento, row["consumo_mes"], row["mg_ml"]))   
     mydb.commit()
