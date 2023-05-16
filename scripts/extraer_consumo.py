e14dc3a0171384de5af0d9c1450e121f557b1533
José Chavarría <112492450+NachoCHR@users.noreply.github.com>
2023-05-16 01:16:29 +0000

    Update extraer_consumo.py

--- scripts/extraer_consumo.py
+++ scripts/extraer_consumo.py
@@ -17,7 +17,7 @@ cursor = mydb.cursor()
 
 # Lectura del archivo CSV de consumo de alimentos
 col_consumo = ["folio", "homologado","consumo_mes", "mg_ml"]
-df_consumo_alimentos = pd.read_csv("../../csv/ENCA_ETCC_ALIMENTOS_INDIVIDUALES.csv", sep=",", header=0, usecols=col_consumo, encoding="ISO-8859-1")
+df_consumo_alimentos = pd.read_csv("../../csv/ENCA_ETCC_ALIMENTOS_INDIVIDUALES.csv", sep=",", header=0, usecols=col_consumo")
 
 
 
