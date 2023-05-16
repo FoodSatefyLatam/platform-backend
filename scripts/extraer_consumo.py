d1b5454cfd62f54fd10d86e57604f0f423c529ce
José Chavarría <112492450+NachoCHR@users.noreply.github.com>
2023-05-16 01:10:30 +0000

    Update extraer_consumo.py

--- scripts/extraer_consumo.py
+++ scripts/extraer_consumo.py
@@ -37,7 +37,7 @@ for index, row in df_consumo_alimentos.iterrows():
     # Obtener el id_alimento a partir del homologado
     cursor.execute("SELECT id FROM Alimento WHERE nombre=%s", (row["homologado"].strip().lower(),))
     id_alimento = cursor.fetchall()[0][0]
-    # print(id_alimento)
+    print(id_alimento)
     # print("Aqui muere")
     
 
