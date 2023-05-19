d668e0c99bdc1a50dd6e4cf05b96cfdc44d7fa53
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-19 19:52:48 -0400

    Update server.py

--- server.py
+++ server.py
@@ -2,11 +2,6 @@ from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 
-import alimentos
-#import calculadora
-import contaminantes
-#import reporte
-
 app = Flask(__name__)
 CORS(app)
 
