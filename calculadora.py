55564688a9407f4b8a17323e2f79b37e3d88595b
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-19 20:16:32 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -20,18 +20,18 @@ def calculadora():
     #aqui saca los datos del contaminante
     contaminante="Cd"
     cur = mysql.connection.cursor()
-    cur.execute("SELECT limite_diario FROM contaminante WHERE nombre= %s",[contaminante]) #de momento se trabaja con el cadmio
+    cur.execute("SELECT limite_diario FROM Contaminante WHERE nombre= %s",[contaminante]) #de momento se trabaja con el cadmio
     valor_referencia = cur.fetchone()
     if(not valor_referencia):
         valor_referencia = [0]
-    cur.execute("SELECT id_contaminante FROM contaminante WHERE nombre= %s",[contaminante])
-    id_contaminante=cur.fetchone()
-    cur.execute("SELECT id_alimento FROM alimento WHERE especie=%s",[food]) 
+    cur.execute("SELECT id FROM Contaminante WHERE nombre= %s",[contaminante])
+    id_contaminante = cur.fetchone()
+    cur.execute("SELECT id FROM Alimento WHERE nombre=%s",[food]) 
     id_alimento=cur.fetchone()
     cur.execute("SELECT Avg(cantidad)  FROM  Muestra WHERE id_contaminante=%s AND id_alimento=%s" ,([id_contaminante],[id_alimento]))
     promedio=cur.fetchone()
     if(not valor_referencia):
         valor_referencia = [0]
-        
+
     formula = (amount * float(promedio[0]))/(float(valor_referencia[0]) * weight) #amount es la cantidad de alimento y weight el peso de la persona
     return jsonify(formula)   
