8e54b13a7dadb8d5a930c944f0d8a8a706381bff
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-19 20:17:15 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -30,8 +30,8 @@ def calculadora():
     id_alimento=cur.fetchone()
     cur.execute("SELECT Avg(cantidad)  FROM  Muestra WHERE id_contaminante=%s AND id_alimento=%s" ,([id_contaminante],[id_alimento]))
     promedio=cur.fetchone()
-    if(not valor_referencia):
-        valor_referencia = [0]
+    if(not promedio):
+        promedio = [0]
 
     formula = (amount * float(promedio[0]))/(float(valor_referencia[0]) * weight) #amount es la cantidad de alimento y weight el peso de la persona
     return jsonify(formula)   
