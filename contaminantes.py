750ea7151a48006b5ded9c27a97da6fcdd65dad3
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-31 01:12:50 -0400

    Update contaminantes.py

--- contaminantes.py
+++ contaminantes.py
@@ -5,10 +5,10 @@ def contaminantes():
     cur = mysql.connection.cursor()
     lista_contaminantes = []
     if request.method == 'GET':
-        cur.execute("SELECT nombre FROM Contaminante")
+        cur.execute("SELECT nombre, alias FROM Contaminante")
         contaminantes = cur.fetchall()
         for contaminante in contaminantes:
-            lista_contaminantes.append(contaminante[0])
+            lista_contaminantes.append({"nombre":contaminante[0],"alias": contaminante[1]})
         
     lista_contaminantes.sort()
     return jsonify(lista_contaminantes)
