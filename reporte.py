0833a18d314a1078924f168cb3270d7f5c5e1c3c
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-09 22:12:00 -0400

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -21,6 +21,13 @@ def reporte():
     contaminantes = request_json.get("contaminantes")
     valores_referencia = {}
     id_contaminantes = {}
+    id_alimentos = {}
+    for alimento in alimentos:
+        cur.execute("SELECT id FROM Alimento WHERE nombre= %s",[alimento])
+        res = cur.fetchall()
+        if(res[0][0]!= ""):
+            id_alimentos[alimento] = res[0][0]
+        
     for contaminante in contaminantes:
         reporte[contaminante] = {}
         reporte[contaminante]["promedio contaminante"] = 0
@@ -31,56 +38,46 @@ def reporte():
         if(res[0][0]!= ""):
             valores_referencia[contaminante] = res[0][0]
             id_contaminantes[contaminante] = res[0][1]
-        else:
-            valores_referencia[contaminante] = 0
-            id_contaminantes[contaminante] = 0
 
     for alimento in alimentos:
-        cur.execute("SELECT id_alimento FROM alimento WHERE especie=%s",[alimento]) 
-        id_alimento=cur.fetchone()[0]
         for contaminante in contaminantes:
             formula = 0.0
             c_personas = 0
             max_formula = 0.0
 
-
             if float(valores_referencia[contaminante]) == 0.0: 
                 continue
 
-            cur.execute("SELECT Avg(cantidad)  FROM  muestreo WHERE id_contaminante=%s AND id_alimento=%s" ,([id_contaminantes[contaminante]],[id_alimento]))
+            cur.execute("SELECT Avg(cantidad)  FROM  Muestra WHERE id_contaminante=%s AND id_alimento=%s" ,([id_contaminantes[contaminante]],[id_alimentos[alimento]]))
             promedio_contaminate = cur.fetchone()[0]
 
             if(promedio_contaminate is None or promedio_contaminate == 0):
                 continue
 
-            cur.execute("SELECT p.peso, consumo.cantidad, p.altura FROM (SELECT * FROM persona WHERE "+ s +" edad > %s AND edad < %s AND peso > %s AND peso < %s AND altura > %s AND altura < %s) AS p LEFT JOIN consumo ON p.id_folio=consumo.id_folio LEFT JOIN alimento ON consumo.id_alimento=alimento.id_alimento WHERE alimento.especie=%s",[min_edad,max_edad,min_peso,max_peso,min_altura,max_altura,alimento])
+            cur.execute("SELECT p.id, p.peso, Consumo.cantidad_mes FROM (SELECT * FROM Persona WHERE "+ s +" edad > %s AND edad < %s AND peso > %s AND peso < %s AND altura > %s AND altura < %s) AS p LEFT JOIN Consumo ON p.id = Consumo.id_persona WHERE Consumo.id_alimento=%s;",[min_edad, max_edad, min_peso, max_peso, min_altura, max_altura, id_alimentos[alimento]])
+
             personas = cur.fetchall()
-            
-            max_formula = {}
-            max_formula["valor formula peor caso"] = 0
-            max_formula["peso peor caso"] = 0
-            max_formula["consumo peor caso"] = 0
-            max_formula["altura peor caso"] = 0
 
             for persona in personas:
-                formula_actual = (float(persona[1]/30) * float(promedio_contaminate))/(float(valores_referencia[contaminante]) * float(persona[0]))
+                formula_actual = (float(persona[2]/30) * float(promedio_contaminate))/(float(valores_referencia[contaminante]) * float(persona[1]))
                 if(formula_actual  > max_formula["valor formula peor caso"]):
-                    max_formula["valor formula peor caso"] = formula_actual
-                    max_formula["peso peor caso"] = persona[0]
-                    max_formula["consumo peor caso"] = persona[1]
-                    max_formula["altura peor caso"] = persona[2]
+                    max_formula= formula_actual
 
                 formula += formula_actual
-                c_personas+=1
-            
-
+                c_personas += 1
             
             if(c_personas != 0):
-                reporte[contaminante]["alimentos"][alimento] = max_formula
-                reporte[contaminante]["alimentos"][alimento]["cantidad de personas"] = c_personas
-                reporte[contaminante]["alimentos"][alimento]["promedio alimento"] = (formula/c_personas)
-                reporte[contaminante]["promedio contaminante"] += (formula/c_personas)
-                reporte[contaminante]["cantidad de alimentos considerados"] += 1
-    
+                reporte[contaminante]["alimento"][alimento]["peor_caso"] = max_formula
+                reporte[contaminante]["alimento"][alimento]["cantidad_de_personas"] = c_personas
+                reporte[contaminante]["alimento"][alimento]["promedio_alimento"] = (formula/c_personas)
+
+
+    for contaminante in contaminantes:
+        promedio_contaminante = 0
+        for alimento  in reporte[contaminante]["alimento"]:
+            promedio_contaminante += alimento["promedio_alimento"]
+        
+        reporte[contaminante]["promedio_contaminante"] = promedio_contaminante
+
     
     return jsonify(reporte)
