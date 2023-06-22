b28a2d0c9f0b1be7037e180afe4b47fff66cda25
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-21 21:59:50 -0400

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -103,7 +103,7 @@ def reporte():
     elif request.method == "GET":
         respuesta = {}
         nombre_alimentos = {}
-        cur.execute("SELECT * FROM Alimentos;")
+        cur.execute("SELECT * FROM Alimento;")
         alimentos = cur.fetchall()
         for alimento in alimentos:
             nombre_alimentos[alimento[0]] = alimento[1]
