1d0207e5a06d1357987e48062c15605b7d764a4d
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-28 17:20:07 -0400

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -17,83 +17,29 @@ def reporte():
         min_peso = request_json["peso"][0]
         max_peso = request_json["peso"][1]
         alimentos = request_json.get("alimentos")
-        contaminantes = request_json.get("contaminantes")
-        valores_referencia = {}
-        id_contaminantes = {}
+        contaminantes = []
+        cur.execute("SELECT id, nombre, limite_diario FROM Contaminante")
+        res = cur.fetchall()
+        for contaminante in res:
+            contaminantes += {"id":contaminante[0], "nombre": contaminante[1], "limite_diario": contaminante[2]} 
+
+        print(contaminantes)
+
         for alimento in alimentos:
             cur.execute("SELECT id FROM Alimento WHERE nombre = %s",[alimento["nombre"]])
             res = cur.fetchall()
+
             if(res[0] is not None):
                 alimento["id"] = res[0][0]
             else:
                 return "error: alimento " + alimento["nombre"] + " no encontrado."
+            
+            for contaminante in contaminantes:
+                if not contaminante["nombre"] in alimento:
+                    cur.execute("SELECT Avg(cantidad)  FROM  Muestra WHERE id_contaminante=%s AND id_alimento=%s" ,([contaminante["id"]],[alimento["id"]]))
         
         print(alimentos)
-            
-        for contaminante in contaminantes:
-            reporte[contaminante] = {}
-            reporte[contaminante]["promedio contaminante"] = 0
-            cur.execute("SELECT limite_diario, id FROM Contaminante WHERE nombre= %s",[contaminante]) #de momento se trabaja con el cadmio
-            res = cur.fetchall()
-            if(res[0] is not None):
-                valores_referencia[contaminante] = res[0][0]
-                id_contaminantes[contaminante] = res[0][1]
         
-        for contaminante in contaminantes:
-            if(not contaminante in id_contaminantes):
-                continue
-
-            reporte[contaminante]["alimento"] = {}
-            for alimento in alimentos:
-
-                reporte[contaminante]["alimento"][alimento] = {}
-                
-                formula = 0.0
-                c_personas = 0
-                max_formula = 0.0
-
-                if  valores_referencia[contaminante] == None or float(valores_referencia[contaminante]) == 0.0: 
-                    continue
-
-                cur.execute("SELECT Avg(cantidad)  FROM  Muestra WHERE id_contaminante=%s AND id_alimento=%s" ,([id_contaminantes[contaminante]],[alimento["id"]]))
-                promedio_contaminante = cur.fetchone()[0]
-
-                if(promedio_contaminante == None):
-                    promedio_contaminante = 0.0
-
-                cur.execute("SELECT p.id, p.peso, Consumo.cantidad_mes FROM (SELECT * FROM Persona WHERE "+ s +" edad > %s AND edad < %s AND peso > %s AND peso < %s) AS p LEFT JOIN Consumo ON p.id = Consumo.id_persona WHERE Consumo.id_alimento=%s AND Consumo.cantidad_mes != 0.0;",[min_edad, max_edad, min_peso, max_peso, alimento["id"]])
-
-                personas = cur.fetchall()
-
-                for persona in personas:
-                    formula_actual = (float(persona[2]/30)/1000.0 * float(promedio_contaminante))/(float(valores_referencia[contaminante]) * float(persona[1]))
-                    if(formula_actual  > max_formula):
-                        max_formula= formula_actual
-
-                    formula += formula_actual
-                    c_personas += 1
-                
-                if(c_personas != 0):
-                    reporte[contaminante]["alimento"][alimento]["peor_caso"] = max_formula
-                    reporte[contaminante]["alimento"][alimento]["cantidad_de_personas"] = c_personas
-                    reporte[contaminante]["alimento"][alimento]["promedio_alimento"] = (formula/c_personas)
-                else:
-                    reporte[contaminante]["alimento"][alimento]["peor_caso"] = 0
-                    reporte[contaminante]["alimento"][alimento]["cantidad_de_personas"] = c_personas
-                    reporte[contaminante]["alimento"][alimento]["promedio_alimento"] = 0
-        
-        print(str(reporte))
-
-        for contaminante in contaminantes:
-            if(not contaminante in id_contaminantes):
-                continue
-            promedio_contaminante = 0
-            for alimento  in alimentos:
-                if("promedio_alimento" in reporte[contaminante]["alimento"][alimento]):
-                    promedio_contaminante += reporte[contaminante]["alimento"][alimento]["promedio_alimento"]
-
-            reporte[contaminante]["promedio contaminante"] = promedio_contaminante
-            
         return jsonify(reporte)
     
     elif request.method == "GET":
