18cd0fa0be9011f715286d41011cb5c7701a2bb0
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-28 18:40:35 -0400

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -92,6 +92,7 @@ def reporte():
                     personas[consumo[0]] = "ok"
                 for contaminante in contaminantes:
                     alimento = filter(lambda _alimento: _alimento['id'] == consumo[3], alimentos)
+                    print(alimento)
                     if(alimento[contaminante["nombre"]] == None):
                         print("sin datos")
                         continue
