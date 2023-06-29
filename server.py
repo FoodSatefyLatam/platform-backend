b1b22e016a8678827a2a9247c5cc9cda317f661c
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-28 23:21:53 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, request, jsonify, send_from_directory
 from flask_mysqldb import MySQL
 from flask_cors import CORS
@@ -7,17 +6,6 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-def print_date_time():
-    dir_list = os.listdir("data")
-    for file in dir_list:
-        file = file[:-5]
-        print(file)
-    
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=print_date_time, trigger="interval", seconds=86400)
-scheduler.start()
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'grupo1'
