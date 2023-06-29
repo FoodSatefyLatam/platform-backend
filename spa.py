c0d4d5392a879a9fce55ad5f6be10cbf26aa4e17
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-29 13:28:17 -0400

    Create spa.py

---
+++ spa.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__, static_folder='app', static_url_path="/app")
+
+if __name__ == "__main__":
+    app.run(port = '5002',  host= '0.0.0.0', debug=False, threaded=True)
+
+@app.route("/heartbeat")
+def heartbeat():
+    return jsonify({"status": "healthy"})
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file("index.html")
