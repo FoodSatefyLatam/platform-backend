7b7bd1571c7768144731b75289b232243e524d0d
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-29 14:38:50 -0400

    Update spa.py

--- spa.py
+++ spa.py
@@ -7,6 +7,7 @@ app = Flask(__name__, static_folder='app', static_url_path="/app")
 def heartbeat():
     return jsonify({"status": "healthy"})
 
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
