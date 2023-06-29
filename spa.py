ab1d95204ae2d77e4b047f914dea58e96d5c37e1
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-29 13:38:41 -0400

    Update spa.py

--- spa.py
+++ spa.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='app', static_url_path="/app")
+
+CORS(app)
 
 if __name__ == "__main__":
     app.run(port = '5002',  host= '0.0.0.0', debug=False, threaded=True)
