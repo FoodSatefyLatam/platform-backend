60d1c0ce5c41d99240aedcf7a2784fba7cb9d530
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-29 14:37:52 -0400

    Update spa.py

--- spa.py
+++ spa.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 
-app = Flask(__name__, static_folder='app', static_url_path="/")
+app = Flask(__name__, static_folder='app', static_url_path="/app")
 
 
 @app.route("/heartbeat")
