ae9504d02c785853ad50f763974ed079b440e74f
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-03 22:15:20 -0400

    Update spa.py

--- spa.py
+++ spa.py
@@ -2,11 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__, static_folder='dist', static_url_path="/")
 
-
 @app.route("/heartbeat")
 def heartbeat():
     return jsonify({"status": "healthy"})
 
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return app.send_static_file("index.html")
