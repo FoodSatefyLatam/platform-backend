939355a99ac5cf762d2969dcd16e14d857e5debd
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-15 00:05:14 -0400

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -16,19 +16,22 @@ def verificar_token_auth0(token):
     json_url = f'https://dev-rqvixarr0an3cp4y.us.auth0.com/.well-known/jwks.json'
     algorithms = ['RS256', 'RS256']
     jwks = requests.get(json_url).json()
+    unverified_header = jwt.get_unverified_header(token)
 
     # Elegir nuestra clave RSA desde el conjunto de claves proporcionadas por Auth0
     rsa_key = {}
     for key in jwks['keys']:
-        rsa_key = {
-            'kty': key['kty'],
-            'kid': key['kid'],
-            'use': key['use'],
-            'n': key['n'],
-            'e': key['e']
-        }
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
     if rsa_key:
         try:
+
             # Verificar la firma del token JWT
             decoded_token = jwt.decode(
                 token,
