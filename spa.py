cb6d4b6cda6124af845eea1094874a4656244c6b
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-17 22:39:14 -0400

    Update spa.py

--- spa.py
+++ spa.py
@@ -11,4 +11,4 @@ def catch_all(path):
     return app.send_static_file("index.html")
 
 if __name__ == "__main__":
-    app.run(port = '5002',  host= '0.0.0.0', debug=False, threaded=True)
+    app.run(port = '5002',  host= '0.0.0.0', debug=False, threaded=True) 
